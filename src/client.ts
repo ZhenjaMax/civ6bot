5d8215f2151b667553f06275eea95ac112de5983
ZhenjaMax <54914800+ZhenjaMax@users.noreply.github.com>
2021-11-09 04:57:23 +0300

    creating example bot module "miscellaneous"

---
+++ src/client.ts
@@ -0,0 +1,42 @@
+import path from "path";
+import { Intents, Interaction, Message } from "discord.js";
+import { Client } from "discordx";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  simpleCommand: {
+    prefix: process.env.BOT_PREFIX,
+  },
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+  classes: [
+    path.join(__dirname, "bot", "**/*.commands.{ts,js}")
+  ],
+  botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+  silent: true,
+});
+
+client.once("ready", async () => {
+  await client.initApplicationCommands({
+    guild:  { log: true },
+    global: { log: true },
+  });
+  await client.initApplicationPermissions(true);  // init permissions; enabled log to see changes
+  console.log("Bot started");
+});
+
+client.on("interactionCreate", (interaction: Interaction) => {
+  client.executeInteraction(interaction);
+});
+
+client.on("messageCreate", (message: Message) => {
+  client.executeCommand(message);
+});
+
+client.login(process.env.BOT_TOKEN || "");
